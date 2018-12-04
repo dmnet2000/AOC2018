8fcdd7b0f4f36511ad3698a5b17f7bb9887000a4
dmnet2000 <dmnet2000@gmail.com>
2018-12-04 23:59:42 +0100

    day1 part1

---
+++ src/it/aoc18/d1/Day1b.java
@@ -0,0 +1,33 @@
+package it.aoc18.d1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Day1b {
+
+
+    public static void main(String[] Args) throws Exception {
+        File f = new File("day1aInput.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(f));
+
+        String line = "";
+
+        int frequenza = 0;
+        while ((line = br.readLine()) != null) {
+            //determino l'operatore
+            char op = line.charAt(0);
+            int val = Integer.parseInt(line.substring(1, line.length()));
+            //System.out.println(val + " " + op);
+            if (op == '+') {
+                frequenza += val;
+            } else {
+                frequenza -= val;
+            }
+            System.out.println(frequenza);
+        }
+    }
+
+}
+
