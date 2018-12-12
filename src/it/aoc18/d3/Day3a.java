fe2bd8dc1805ce386aa50dc3e2af580983ec19c9
dmnet2000 <dmnet2000@gmail.com>
2018-12-13 00:07:57 +0100

    day3b

--- src/it/aoc18/d3/Day3a.java
+++ src/it/aoc18/d3/Day3a.java
@@ -3,8 +3,7 @@ package it.aoc18.d3;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Day3a {
     public static void main(String[] Args) throws Exception {
@@ -21,10 +20,10 @@ public class Day3a {
             listaRighe.add(line);
         }
         //TODO:rimuovere
-        listaRighe = new ArrayList<>();
+        /*listaRighe = new ArrayList<>();
         for (int i = 0; i < test.length; i++) {
             listaRighe.add(test[i]);
-        }
+        }*/
         List<Claim> listaClaim = new ArrayList<>();
         for (String l : listaRighe) {
             Claim claim = new Claim();
@@ -53,26 +52,32 @@ public class Day3a {
 
         int count = 0;
 
+        Map<String, Claim> id = new HashMap<>();
         for (Claim claim : listaClaim) {
             for (int j = claim.top; j < claim.top + claim.altezza; j++) {
                 for (int i = claim.sx; i < claim.larghezza + claim.sx; i++) {
                     if (matrix[j][i].equals(".")) {
                         matrix[j][i] = claim.id;
                     } else {
-                        matrix[j][i] = "X";
+
+                        id.put(matrix[j][i], new Claim());
                         count++;
+                        id.put(claim.id, claim);
+                        matrix[j][i] = "X";
                     }
                 }
             }
 
         }
 
+        int count1 = 0;
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
                 if (matrix[i][j].equals(".")) {
                     System.out.print("  . ");
                 } else if (matrix[i][j].equals("X")) {
                     System.out.print("  X ");
+                    count1++;
                 } else {
                     System.out.print(matrix[i][j]);
 
@@ -83,7 +88,27 @@ public class Day3a {
             }
             System.out.print("\n");
         }
-        System.out.print("conflitti = " + count);
+        System.out.println("conflitti = " + count);
+        System.out.println("conflitti = " + count1);
+
+        Set<String> keySet = id.keySet();
+        //keySet.forEach(key -> System.out.println(key));
+
+
+        for (Claim claim : listaClaim) {
+            boolean trovato = false;
+            for (String key : keySet) {
+                if (claim.id.equals(key)) {
+                    trovato = true;
+                    //escludere conflitto
+                    //System.out.println("Trovato = " + claim.id);
+                    break;
+                }
+            }
+            if (!trovato) {
+                System.out.print("ID PULITO=" + claim.id);
+            }
+        }
     }
 
     static class Claim {
@@ -99,7 +124,7 @@ public class Day3a {
 
         @Override
         public java.lang.String toString() {
-            return "" + id + "->" + sx + "-" + top + "|"+ larghezza + "-"+ altezza;
+            return "" + id + "->" + sx + "-" + top + "|" + larghezza + "-" + altezza;
         }
     }
 
