5a70612a8bc87f995120c28cf20f0a667a5a08a7
dmnet2000 <dmnet2000@gmail.com>
2018-12-12 23:40:52 +0100

    day3a

---
+++ src/it/aoc18/d3/Day3a.java
@@ -0,0 +1,106 @@
+package it.aoc18.d3;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day3a {
+    public static void main(String[] Args) throws Exception {
+        File f = new File("day3Input.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(f));
+
+        String line = "";
+        List<String> listaRighe = new ArrayList<>();
+
+        String test[] = {"#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"};
+
+        while ((line = br.readLine()) != null) {
+            listaRighe.add(line);
+        }
+        //TODO:rimuovere
+        listaRighe = new ArrayList<>();
+        for (int i = 0; i < test.length; i++) {
+            listaRighe.add(test[i]);
+        }
+        List<Claim> listaClaim = new ArrayList<>();
+        for (String l : listaRighe) {
+            Claim claim = new Claim();
+            String id = l.substring(l.indexOf("#") + 1, l.indexOf("@") - 1);
+
+            claim.id = String.format("%04d", Integer.parseInt(id));
+            claim.sx = Integer.parseInt(l.substring(l.indexOf("@") + 2, l.indexOf(",")));
+            claim.top = Integer.parseInt(l.substring(l.indexOf(",") + 1, l.indexOf(":")));
+            claim.larghezza = Integer.parseInt(l.substring(l.indexOf(":") + 2, l.indexOf("x")));
+            claim.altezza = Integer.parseInt(l.substring(l.indexOf("x") + 1, l.length()));
+
+            System.out.println(claim);
+            listaClaim.add(claim);
+        }
+
+        int length = 1020;
+        //int length = 8;
+        String[][] matrix = new String[length][length];
+
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                matrix[i][j] = ".";
+            }
+        }
+
+        int count = 0;
+
+        for (Claim claim : listaClaim) {
+            for (int j = claim.top; j < claim.top + claim.altezza; j++) {
+                for (int i = claim.sx; i < claim.larghezza + claim.sx; i++) {
+                    if (matrix[j][i].equals(".")) {
+                        matrix[j][i] = claim.id;
+                    } else {
+                        matrix[j][i] = "X";
+                        count++;
+                    }
+                }
+            }
+
+        }
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (matrix[i][j].equals(".")) {
+                    System.out.print("  . ");
+                } else if (matrix[i][j].equals("X")) {
+                    System.out.print("  X ");
+                } else {
+                    System.out.print(matrix[i][j]);
+
+                }
+                if (j < length - 1) {
+                    System.out.print("|");
+                }
+            }
+            System.out.print("\n");
+        }
+        System.out.print("conflitti = " + count);
+    }
+
+    static class Claim {
+        public String id;
+        public int sx;
+        public int top;
+        public int larghezza;
+        public int altezza;
+
+        public Claim() {
+        }
+
+
+        @Override
+        public java.lang.String toString() {
+            return "" + id + "->" + sx + "-" + top + "|"+ larghezza + "-"+ altezza;
+        }
+    }
+
+}
