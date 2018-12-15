5702d8c836c2f416a986bb24f2ca6db8bd32c5ea
dmnet2000 <dmnet2000@gmail.com>
2018-12-15 01:33:34 +0100

    WORK IN PROGRESS DAY4

--- src/it/aoc18/d4/Day4a.java
+++ src/it/aoc18/d4/Day4a.java
@@ -82,27 +82,127 @@ public class Day4a {
         operationList.forEach(op -> System.out.println("ora:" + op.data + "|ora=" + op.ora + "|id=" + op.id + "| op=" + op.operation));
         Map<String, Integer> mappaIdSleep = new HashMap<>();
         Map<String, Integer> mappaIdNotSleep = new HashMap<>();
-        String id = "";
-        int minSleep = 0;
-        int minUp = 0;
+        id = "";
+        int oraStart = 0;
+        int minStart = 0;
+        Stato st = Stato.NOP;
         for (Operation op : operationList) {
             if (op.operation.contains("begins shift")) {
                 id = op.id;
+                if (mappaIdSleep.get(id) == null) {
+                    mappaIdSleep.put(id, 0);
+
+                }
+                st = Stato.START;
             }
-            if (id.equals(op.id)) {
-             if (mappaIdSleep.get(id)==null){
-                 //mappaIdSleep.put(id, )
-             }
+            if (op.operation.contains("falls asleep") && (st == Stato.START || st == Stato.END_SLEEP)) {
+                oraStart = Integer.parseInt(op.ora.substring(0, op.ora.indexOf(":")));
+                minStart = Integer.parseInt(op.ora.substring(op.ora.indexOf(":") + 1, op.ora.length()));
+                st = Stato.START_SLEEP;
+
             }
-            int ora = Integer.parseInt(op.ora.substring(0, op.ora.indexOf(":")));
-            int min = Integer.parseInt(op.ora.substring(0, op.ora.indexOf(":")));
+            if (op.operation.contains("wakes up") && st == Stato.START_SLEEP) {
+                int oraEnd = Integer.parseInt(op.ora.substring(0, op.ora.indexOf(":")));
+                int minEnd = Integer.parseInt(op.ora.substring(op.ora.indexOf(":") + 1, op.ora.length()));
+                int value = mappaIdSleep.get(id);
+
+                if (oraEnd == oraStart) {
+                    value = value + minEnd - minStart;
+                    String[] minuti = new String[60];
+                    for (int i = minStart; i <= minEnd; i++) {
+                        minuti[i] = "X";
+                    }
+                    op.listaMinuti.add(minuti);
+                } else {
+                    if (oraEnd - oraStart > 1) {
+                        value = value + (((oraEnd - oraStart) - 1) * 60) + (60 - minStart) + minEnd;
+                        for (int i = 0; i < oraEnd - oraStart; i++) {
+                            String[] ora = new String[60];
+                            for (int j = 0; j <= 59; i++) {
+                                ora[j] = "X";
+                            }
+                            op.listaMinuti.add(ora);
+                        }
+                        String[] minuti = new String[60];
+                        for (int i = minStart; i <= minEnd; i++) {
+                            minuti[i] = "X";
+                        }
+                        op.listaMinuti.add(minuti);
+                    } else {
+                        value = value + (60 - minStart) + minEnd;
+                        String[] minuti = new String[60];
+                        for (int i = minStart; i <= minEnd; i++) {
+                            minuti[i] = "X";
+                        }
+                        String[] ora = new String[60];
+                        for (int i = 0; i <= 59; i++) {
+                            ora[i] = "X";
+                        }
+                        op.listaMinuti.add(minuti);
+                        op.listaMinuti.add(ora);
+                    }
+                }
+
+                mappaIdSleep.put(id, value);
+                st = Stato.END_SLEEP;
+            }
         }
+        String[] idMaxSLeep = {""};
+        int value = 0;
+        Set<String> key = mappaIdSleep.keySet();
+        for (String k : key) {
+            System.out.println("id=" + k + "value=" + mappaIdSleep.get(k));
+            if (mappaIdSleep.get(k) > value) {
+                value = mappaIdSleep.get(k);
+                idMaxSLeep[0] = k;
+            }
+
+        }
+
+        int[] minMaxSlip = new int[60];
+        for (int i = 0; i < 60; i++) {
+            minMaxSlip[i] = 0;
+        }
+
+        for (Operation op : operationList) {
+            if (op.id.equals(idMaxSLeep[0])) {
+                for (String[] minuti : op.listaMinuti) {
+                    for (int i = 0; i < 60; i++) {
+                        if (minuti[i].equals("X")) {
+                            minMaxSlip[i] = minMaxSlip[i] + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        int index = 0;
+        int val = 0;
+        for (int i = 0; i < 60; i++) {
+            if (minMaxSlip[i] > val) {
+                val = minMaxSlip[i];
+                index = i;
+            }
+        }
+
+        System.out.print("result = " + Integer.parseInt(idMaxSLeep[0]) * index);
     }
+
 
     static class Operation {
         public Date data;
         public String operation;
         public String id;
         public String ora;
+        public List<String[]> listaMinuti;
+
+        public Operation() {
+            listaMinuti = new ArrayList<>();
+        }
     }
+
+    private enum Stato {
+        START, START_SLEEP, END_SLEEP, NOP;
+    }
+
 }
